a12edd07ff8a2acda09ce247a33c10ca22817932
Ramis Cesar Damasceno <rdamasce@c2r3p6.42sp.org.br>
2025-08-26 20:56:31 -0300

    print char done

--- ft_printf.c
+++ ft_printf.c
@@ -6,46 +6,58 @@
 /*   By: rdamasce <rdamasce@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/20 22:13:30 by rdamasce          #+#    #+#             */
-/*   Updated: 2025/08/20 22:58:23 by rdamasce         ###   ########.fr       */
+/*   Updated: 2025/08/26 20:55:35 by rdamasce         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
+#include <unistd.h>
 
-int ft_printf(const char* format_string, ...)
+void	ft_print_string(const char *c);
+void	ft_print_char(char c);
+
+int	ft_printf(const char* format_string, ...)
 {
-	int count_args;
-	int i;
-	va_list args;
-
-	count_args = 0;
-	i = 0;
-	while (format_string[i])
-	{
-		if(format_string[i] == '%')
-			count_args++;
-		i++;
-	}
-	printf("count_args: %d\n", count_args);
+	int		j;
+	char	c;
+	va_list	args;
 
 	va_start(args, format_string);
 
-	i = 0;
-	while (i < count_args)
+	j = 0;
+	while (format_string[j] != '\0')
 	{
-		int num = va_arg(args, int);
-		printf("%d", num);
-		i++;
+		if (format_string[j] == '%')
+		{
+			j++;
+			if (format_string[j] == 'c')
+			{
+				c = va_arg(args, int);
+				ft_print_char(c);
+			}
+			else if (format_string[j] == 's')
+			{
+				c = va_arg(args, int);
+				ft_print_string(c);
+			}
+			// else if para diferentes formatos
+		}
+		else
+		{
+			write(1, &format_string[j], 1);
+		}
+		j++;
 	}
-	printf("%s", format_string);
+	va_end(args);
 	return (0);
 }
 
 int main(void)
 {
-	const char* format_string = "valor de i: %d, valor de j %d";
-	int i = 5;
-	int j = 6;
-	ft_printf(format_string, i, j);
+	const char* format_string = "primerira letra: %c, segunda letra %c, primeira palavra %s";
+	char str[] = "palavra";
+	char a = 'a';
+	int b = 'b';
+	ft_printf(format_string, a, b, str);
 	return (0);
 }
